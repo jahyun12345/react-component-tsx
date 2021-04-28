448bfc5d026c688deae4e6dd78b386f91d380de0
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-04-28 16:58:55 +0900

    * class component life cycle console log testing

--- src/component/ClassComp.tsx
+++ src/component/ClassComp.tsx
@@ -16,6 +16,30 @@ export default class ClassComp extends React.Component<ClassProps, ClassState> {
     _date:(new Date()).toString()
   }
 
+  classStyle = 'color:red';
+  // render 이전 실행
+  componentWillMount() {
+    console.log('%cclass => componentWillMount', this.classStyle);
+  }
+  // render 이후 실행
+  componentDidMount() {
+    console.log('%cclass => componentDidMount', this.classStyle);
+  }
+  // render 호출할 필요가 있는지 결정
+  // true : render 호출 / false : render 호출 X
+  shouldComponentUpdate(nextProps:any, nextState:any) {
+    console.log('%cclass => shouldComponentUpdate', this.classStyle);
+    return true;
+  }
+  // 상태 변경 후 render 이전 실행
+  componentWillUpdate(nextProps:any, nextState:any) {
+    console.log('%cclass => componentWillUpdate', this.classStyle);
+  }
+  // 상태 변경 후 render 이후 실행
+  componentDidUpdate(nextProps:any, nextState:any) {
+    console.log('%cclass => componentDidUpdate', this.classStyle);
+  }
+
   clickEventHandle() {
     this.setState({number:Math.random()});
   }
@@ -23,6 +47,7 @@ export default class ClassComp extends React.Component<ClassProps, ClassState> {
   // state 값이 바뀜에 따라 render() 호출
   // => 바뀐 결과 반영
   render() {
+    console.log('%cclass => render', this.classStyle);
     return (
       <div className="container">
         <h2>class style component</h2>
